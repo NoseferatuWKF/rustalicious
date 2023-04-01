d41c272281c3e83d065f87c560ff965e4ce3cfc5
NoseferatuWKF <wkf2584@gmail.com>
2023-04-01 16:47:42 +0800

    feet: binary search

--- dsa/src/main.rs
+++ dsa/src/main.rs
@@ -1,3 +1,9 @@
+mod binary_search;
+
+use binary_search::sorted_binary_search;
+
 fn main() {
-    println!("Hello, world!");
+    let haystack: Vec<usize> = vec![5, 13, 14, 21, 32, 44];
+    let res = sorted_binary_search(&14, &haystack);
+    println!("{res}");
 }
