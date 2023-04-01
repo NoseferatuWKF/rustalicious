71e65f98a072a390f620a26e8b9c6314348ba492
NoseferatuWKF <wkf2584@gmail.com>
2023-04-01 21:59:07 +0800

    feet: binary search
    still have to figure out how recursion works in rust

--- dsa/src/main.rs
+++ dsa/src/main.rs
@@ -1,9 +1,5 @@
-mod binary_search;
-
-use binary_search::sorted_binary_search;
+mod algorithms;
+mod data_structures;
 
 fn main() {
-    let haystack: Vec<usize> = vec![5, 13, 14, 21, 32, 44];
-    let res = sorted_binary_search(&14, &haystack);
-    println!("{res}");
 }
